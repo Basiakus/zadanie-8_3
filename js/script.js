a166161b0955061798b15d7c91c6bbfa28e06d2e
Adam Basiak <basiakus@gmail.com>
2017-10-27 17:34:27 +0200

    add all project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var zmienna = prompt("Grałeś kiedyś w Mario?");
+alert(zmienna + " ?");
+
+
+var zmienna2 = prompt("to tak, czy nie???");
+console.log(zmienna2);
+alert("skoro " + zmienna2 + " to " + zmienna2);
