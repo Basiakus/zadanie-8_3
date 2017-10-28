6ef2d9789445527bce70dc1e8b2c72bf4797ae4b
Adam Basiak <basiakus@gmail.com>
2017-10-28 20:28:22 +0200

    Exercise with triangle area is on branch 'trangleField'

--- js/script.js
+++ js/script.js
@@ -18,3 +18,12 @@ function mario () {
 	}
 } 
 */
+alert('Spróbujmy napisać prosty "program", który obiczy pole trójkąta na podstawie Twoich danych. ZACZYNAMY!');
+
+var triangleBasis = prompt('Wpisz wartość całkowitą długości jego podstawy w cm.');
+console.log(triangleBasis);
+var triangleHeight = prompt("Potrzebujemy jescze jego wysokość, również w cm.");
+console.log(triangleHeight);
+var triangleField = ((triangleBasis*triangleHeight)/2);
+alert('Na podstawie Twoich danych pole trójkąta, którego podstawa wynosi: ' + triangleBasis + 'cm. ,oraz wysokość: ' + triangleHeight + 'cm. jest równe: ' + triangleField + "cm\u00B2.");
+console.log('Pole trójkąta wynosi: ' + triangleField + ' cm \u00B2');
