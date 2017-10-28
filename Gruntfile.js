d370d6e05d083d1710487fde8ca42a86c497752e
Adam Basiak <basiakus@gmail.com>
2017-10-28 19:52:15 +0200

    Gruntfile.js has formated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    sass: {                              // Task 
-        dist: {                            // Target 
-          options: {                       // Target options 
-            style: 'expanded'
-          },
-          files: {                         // Dictionary of files 
-            'css/style.css': 'css/style.scss'     // 'destination': 'source' 
-         }   
-        }
-  },
-    jshint: {
-    all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-        css: {
-          files: '**/*.scss',
-          tasks: ['sass'],
-          options: {
-            livereload: true,
-          },
+    // Project configuration.
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
         },
-    },
-  });
+        jshint: {
+            all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass'],
+                options: {
+                    livereload: true,
+                },
+            },
+        },
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', ['sass', 'jshint', 'watch']);
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'jshint', 'watch']);
 
 };
