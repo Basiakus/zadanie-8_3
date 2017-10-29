a391f8056fa9cd7e236955f3ca0ffb9da77b4260
Adam Basiak <basiakus@gmail.com>
2017-10-29 14:34:13 +0100

    comments deleted

--- js/script.js
+++ js/script.js
@@ -5,16 +5,3 @@ alert(zmienna + " ?");
 var zmienna2 = prompt("to tak, czy nie???");
 console.log(zmienna2);
 alert("skoro " + zmienna2 + " to " + zmienna2);
-//próbowałem tutaj napisać jakąś funkcję, 
-//która wykorzystywała by if, else np.
-
-/*
-function mario () { 
-	var zmienna2;
-	if(znienna2 == "tak") {
-	alert("Jump!!!");
-	} else {
-	alert("...najlepiej na nintendo :D");
-	}
-} 
-*/
