481afae7e227d9da9a20fe4f7047a6caf7817c68
Adam Basiak <basiakus@gmail.com>
2017-10-29 13:38:04 +0100

    next exercise with operators and mario complited

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-var zmienna = prompt("Grałeś kiedyś w Mario?");
-alert(zmienna + " ?");
-
-
-var zmienna2 = prompt("to tak, czy nie???");
-console.log(zmienna2);
-alert("skoro " + zmienna2 + " to " + zmienna2);
-//próbowałem tutaj napisać jakąś funkcję, 
-//która wykorzystywała by if, else np.
-
-/*
-function mario () { 
-	var zmienna2;
-	if(znienna2 == "tak") {
-	alert("Jump!!!");
-	} else {
-	alert("...najlepiej na nintendo :D");
-	}
-} 
-*/
 alert('Do obliczenia kwadratu różnicy (a - b\)\u00B2 potrzebujemy zmiennej a i b.');
 
 var numberA = prompt('Wpisz wartość liczby a');
@@ -29,28 +9,101 @@ alert('Kwadrat róźnicy = ' + squareMinus);
 console.log(squareMinus);
 alert('instrukcja warunkowa "if - else if - else"');
 if (squareMinus > 0) {
-	alert('Wynik jest dodatni');
-	console.log("wynik dodatni");
+    alert('Wynik jest dodatni');
+    console.log("wynik dodatni");
 } else if (squareMinus < 0) {
-	alert('Wynik jest ujemny');
-	console.log('Wynik ujemny');
+    alert('Wynik jest ujemny');
+    console.log('Wynik ujemny');
 } else {
-	alert('Wynik jest równy 0');
-	console.log('Wynik równy 0');
+    alert('Wynik jest równy 0');
+    console.log('Wynik równy 0');
 }
 alert("Próba z Switch'em");
 switch (squareMinus) {
-	case '0':
-		alert("Switch mówi, że case jest = 0");
-		console.log("Switch case '0':");
-		break;
-	default:
-		alert("pozostał tylko default z info: 'Liczba różna od 0'");
-		console.log('Default: != 0');
+    case '0':
+        alert("Switch mówi, że case jest = 0");
+        console.log("Switch case '0':");
+        break;
+    default:
+        alert("pozostał tylko default z info: 'Liczba różna od 0'");
+        console.log('Default: != 0');
 
 }
 alert("Na koniec 'short if'");
 (squareMinus > 0) ? alert("'short-if:true' Liczba jest dodatnia")
 
-	:
-	alert("'short-if:false' Liczba jest ujemna");
+    :
+    alert("'short-if:false' Liczba jest ujemna");
+
+alert("Wróćmy jeszcze do Pana Mario i spróbujmy z switch'em");
+
+
+
+
+var zmienna = prompt("Grałeś kiedyś w Mario?");
+console.log(zmienna);
+
+switch (zmienna) {
+    case 'tak':
+        alert('No to JUMP !!!');
+        break;
+    case 'nie':
+        alert('Najlepiej na Nintendo');
+        break;
+    default:
+        alert("Wpisz 'tak' / 'nie'");
+alert('Do obliczenia kwadratu różnicy (a - b\)\u00B2 potrzebujemy zmiennej a i b.');
+
+var numberA = prompt('Wpisz wartość liczby a');
+var numberB = prompt('Wpisz wartość liczby b');
+
+alert('wartość liczby a = ' + numberA + ', liczby b = ' + numberB);
+var squareMinus = ((numberA * numberA) - (2 * numberA * numberB) + (numberB * numberB));
+alert('Kwadrat róźnicy = ' + squareMinus);
+console.log(squareMinus);
+alert('instrukcja warunkowa "if - else if - else"');
+if (squareMinus > 0) {
+    alert('Wynik jest dodatni');
+    console.log("wynik dodatni");
+} else if (squareMinus < 0) {
+    alert('Wynik jest ujemny');
+    console.log('Wynik ujemny');
+} else {
+    alert('Wynik jest równy 0');
+    console.log('Wynik równy 0');
+}
+alert("Próba z Switch'em");
+switch (squareMinus) {
+    case '0':
+        alert("Switch mówi, że case jest = 0");
+        console.log("Switch case '0':");
+        break;
+    default:
+        alert("pozostał tylko default z info: 'Liczba różna od 0'");
+        console.log('Default: != 0');
+
+}
+alert("Na koniec 'short if'");
+(squareMinus > 0) ? alert("'short-if:true' Liczba jest dodatnia")
+
+    :
+    alert("'short-if:false' Liczba jest ujemna");
+
+alert("Wróćmy jeszcze do Pana Mario i spróbujmy z switch'em");
+
+
+
+
+var zmienna = prompt("Grałeś kiedyś w Mario?");
+console.log(zmienna);
+
+switch (zmienna) {
+    case 'tak':
+        alert('No to JUMP !!!');
+        break;
+    case 'nie':
+        alert('Najlepiej na Nintendo');
+        break;
+    default:
+        alert("Wpisz 'tak' / 'nie'");
+}
