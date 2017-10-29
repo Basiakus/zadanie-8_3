091ab5892806213fdf45e1a7a1aca4f6fc9c4f11
Adam Basiak <basiakus@gmail.com>
2017-10-29 13:20:25 +0100

    next exercise in script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,39 @@ function mario () {
 	}
 } 
 */
+alert('Do obliczenia kwadratu różnicy (a - b\)\u00B2 potrzebujemy zmiennej a i b.');
+
+var numberA = prompt('Wpisz wartość liczby a');
+var numberB = prompt('Wpisz wartość liczby b');
+
+alert('wartość liczby a = ' + numberA + ', liczby b = ' + numberB);
+var squareMinus = ((numberA * numberA) - (2 * numberA * numberB) + (numberB * numberB));
+alert('Kwadrat róźnicy = ' + squareMinus);
+console.log(squareMinus);
+alert('instrukcja warunkowa "if - else if - else"');
+if (squareMinus > 0) {
+	alert('Wynik jest dodatni');
+	console.log("wynik dodatni");
+} else if (squareMinus < 0) {
+	alert('Wynik jest ujemny');
+	console.log('Wynik ujemny');
+} else {
+	alert('Wynik jest równy 0');
+	console.log('Wynik równy 0');
+}
+alert("Próba z Switch'em");
+switch (squareMinus) {
+	case '0':
+		alert("Switch mówi, że case jest = 0");
+		console.log("Switch case '0':");
+		break;
+	default:
+		alert("pozostał tylko default z info: 'Liczba różna od 0'");
+		console.log('Default: != 0');
+
+}
+alert("Na koniec 'short if'");
+(squareMinus > 0) ? alert("'short-if:true' Liczba jest dodatnia")
+
+	:
+	alert("'short-if:false' Liczba jest ujemna");
